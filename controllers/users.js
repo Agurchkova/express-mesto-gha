0083d4e7d6516c3ef805e815bbb403ec4d51f6b8
Agurchkova <copyart@mail.ru>
2023-05-09 21:34:56 +0300

    ref5

--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,7 @@ const {
   OK,
 } = require('../utils/constants');
 
-const { NODE_ENV, JWT_SECRET } = require('../config');
+const { NODE_ENV, JWT_SECRET, jwtKey } = require('../config');
 
 // User authentication
 module.exports.login = (req, res, next) => {
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
       // создание токена
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        NODE_ENV === 'production' ? JWT_SECRET : jwtKey,
         {
           expiresIn: '7d',
         },
